4f44f7eb6c7f37399e9bb2cf91d292733a9ebf6f
sidneiJr2017 <sidnei.ifrn@gmail.com>
2019-12-11 12:21:19 -0300

    :construction: segundo modelo de site pessoal

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menuP = document.getElementsByTagName('header')[0]
+const fotoPerfil = document.getElementsByClassName('nav-photo')[0]
+
+//  console.log('aqui', menuP)
+//  console.log('aqui', menuP.clientHeight)
+
+// fotoPerfil.style.width  = `${0.7 * menuP.clientHeight}px`
+// fotoPerfil.style.height  = `${0.7 * menuP.clientHeight}px`
+// fotoPerfil.style.width = `250px`
+
